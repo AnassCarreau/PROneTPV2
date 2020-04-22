020e02ad913e91fb6760cc2786b7e30da3c5b628
raulblas7 <raulblas@ucm.es>
2020-04-22 17:23:16 +0200

    lol

--- TPV2/TPV2/CollisionSystem.h
+++ TPV2/TPV2/CollisionSystem.h
@@ -7,25 +7,29 @@
 #include "FighterSystem.h"
 #include "BulletsSystem.h"
 #include "Transform.h"
-
+#include "ScoreManager.h"
 class CollisionSystem: public System {
 public:
 
 	void update() {
-		auto ptr = mngr_->getHandler<_hdlr_Fighter>()->getComponent<Transform>();
-		for( auto& e : mngr_->getGroupEntities<_grp_Asteroid>()) {
-			auto etr = e->getComponent<Transform>();
-			if ( Collisions::collides(ptr->position_, ptr->width_, ptr->height_, etr->position_, etr->width_, etr->height_)) {
-				mngr_->getSystem<FighterSystem>()->onCollisionWithAsteroid(e);
-			}
-			for (auto& b : mngr_->getGroupEntities<_grp_Bullet>()) {
-				auto bull = b->getComponent<Transform>();
-				if (Collisions::collides(etr->position_, etr->width_, etr->height_, bull->position_, bull->width_, bull->height_)) {
-					mngr_->getSystem<AsteroidsSystem>()->onCollisionWithBullet(e, b);
-					mngr_->getSystem<BulletsSystem>()->onCollisionWithAsteroid(b, e);
+		if (!scoreManager_->getPause()) {
+			auto ptr = mngr_->getHandler<_hdlr_Fighter>()->getComponent<Transform>();
+			for (auto& e : mngr_->getGroupEntities<_grp_Asteroid>()) {
+				auto etr = e->getComponent<Transform>();
+				if (Collisions::collides(ptr->position_, ptr->width_, ptr->height_, etr->position_, etr->width_, etr->height_)) {
+					mngr_->getSystem<FighterSystem>()->onCollisionWithAsteroid(e);
+				}
+				for (auto& b : mngr_->getGroupEntities<_grp_Bullet>()) {
+					auto bull = b->getComponent<Transform>();
+					if (Collisions::collides(etr->position_, etr->width_, etr->height_, bull->position_, bull->width_, bull->height_)) {
+						mngr_->getSystem<AsteroidsSystem>()->onCollisionWithBullet(e, b);
+						mngr_->getSystem<BulletsSystem>()->onCollisionWithAsteroid(b, e);
+					}
 				}
 			}
 		}
 	}
+private:
+	ScoreManager* scoreManager_;
 };
 
